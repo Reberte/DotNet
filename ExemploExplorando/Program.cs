9a50b7c1de3f9ccc2ed3c3896e2826196f8be4c9
Reberte <marcos.reberte@gmail.com>
2024-05-28 18:27:40 -0300

    Continuando estudos em C# DotNet

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -4,20 +4,275 @@ using System.Data;
 using System.Globalization;
 
 
+
+// IF  TERNARIO 
+
+int numero = 20;
+bool ehPar = false;
+
+// IF TERNÁRIO
+
+ehPar = numero % 2 == 0;
+
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
+
+
+/// if normal
+// if (numero % 2 == 0)
+// {
+//   Console.WriteLine($"O número {numero} é par ");
+// }
+// else 
+// {
+//   Console.WriteLine($"O número {numero} é impar");
+// }
+
+
+
+
+
+
+/*
+//Desconstrutor metodo criado na classe Pessoa
+Pessoa p1 = new Pessoa("Marcos", "Reberte");
+
+(string nome, string sobrenome) = p1;
+
+Console.WriteLine($"{nome} {sobrenome}");
+
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+//TUPLA coleção de dados que pode armazenar varias dados de tipos diferentes
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+
+if (sucesso)
+{
+  foreach(string linha in linhasArquivo)
+  {
+    Console.WriteLine(linha);
+  }
+
+}
+else 
+{
+  Console.WriteLine("Não foi posível ler o arquivo");
+}
+
+
+*/
+
+/*
+
+//TUPLA coleção de dados que pode armazenar varias dados de tipos diferentes
+
+//forma recomendada de declarar um Tupla
+(int, string, string, decimal) tupla = (1, "Marcos", "Reberte", 10.5M);
+
+//ValeuTuple<int, string, string, decimal> outroExemplo = (1, "Marcos", "Reberte", 10.5M);
+//var outroExemploTuplaCreate = Tupla.Create(1, "Marcos", "Reberte", 10.5M);
+
+Console.WriteLine($"Id: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
+
+
+
+*/
+
+
+
+/*
+
+// Dictionary <chave, elemento> a chave deve ser unica, não pode ter repetido
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach(var item in estados)
+{
+  Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("------------------");
+
+estados.Remove("BA"); // removendo Bahia informando a chave
+
+estados["SP"] = "São Paulo - valor alterado";
+
+foreach(var item in estados)
+{
+  Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("------------------");
+
+string chave = "BA";
+
+Console.WriteLine($"Verificando o elemento {chave}");
+
+if (estados.ContainsKey(chave))
+{
+  Console.WriteLine($"Valor existente:  {chave}");
+}
+else
+{
+  Console.WriteLine($"Valor não existe. É seguro adicionar a chave:  {chave}");
+}
+
+
+// Consultando um estado
+
+Console.WriteLine(estados["MG"]);
+
+
+*/
+
+
+
+
+
+
+/*
+// Stack Pilha LIFO o O primeiro a entrar na pilha será o último a sair
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach(int item in pilha)
+{
+  Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento do topo {pilha.Pop()}");
+
+pilha.Push(20);
+
+foreach(int item in pilha)
+{
+  Console.WriteLine(item);
+}
+
+*/
+
+
+
+
+
+
+/*
+// Queue FILA FIFO -> primeiro que entrar na fila de execução será o primeiro a sair da fila;
+
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+fila.Enqueue(10);
+fila.Enqueue(12);
+
+foreach(int item in fila)
+{
+  Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+
+foreach(int item in fila)
+{
+  Console.WriteLine(item);
+}
+
+
+*/
+
+
+
+
+
+
+
+/*
+// Usando throw -> tratando uma execeção
+
+new ExemploExcecao().Metodo1();
+
+*/
+
+
+
+
+
+
+
+
+
+/*
 ///LENDO ARUIVO DE TEXTO
 ///
 // try executa o código com cautela, se tiver erro o catch pega e cria uma exceção com o Exception 
+//finally {} sempre será executado, dando erro ou não no código.
+
 try{ 
-  string[] linhas = File.ReadAllLines("Arquivos/aarquivoLeitura.txt");
+  string[] linhas = File.ReadAllLines("Arquivoss/aarquivoLeitura.txt");
 
  foreach(string linha in linhas)
  {
   Console.WriteLine(linha);
  }
-}catch(Exception ex)
+}
+catch(FileNotFoundException ex)
 {
   Console.WriteLine($"Ocorreu um erro na leitura do arquivo {ex.Message}");
 }
+
+catch(DirectoryNotFoundException ex)
+{
+  Console.WriteLine("Ocorreu um erro na leitura do arquivo. Caminho da Pasta não encontrado. " + ex.Message);
+}
+
+catch(Exception ex)
+{
+  Console.WriteLine($"Ocorreu exceção Genérica {ex.Message}");
+}
+
+finally
+{
+  Console.WriteLine("Chegou até aqui!");
+}
+
+
+
+*/
+
+
+
+
+
+
+
+
 
 /*
 //// MANIPULANDO E FORMATANDO DATAS
