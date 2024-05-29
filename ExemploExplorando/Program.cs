ad5173aedf5c1ccd33cfd6fdadc9daa4622e2c9b
Reberte <marcos.reberte@gmail.com>
2024-05-29 19:27:28 -0300

    Novos exemplos e estudo de C#

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -2,8 +2,63 @@
 using ExemploExplorando.Models;
 using System.Data;
 using System.Globalization;
+using Newtonsoft.Json;
 
 
+
+
+
+
+
+
+//Serializar e Deserializar
+
+
+//Deserializar pegar o Json e transformar em Objeto
+
+string consteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(consteudoArquivo);
+
+foreach (Venda venda in listaVenda)
+{
+  Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}" +
+                    $" Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
+
+
+
+
+
+/*//Serializar Gerar um aquivo Json
+DateTime dataAtual = DateTime.Now;
+
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de escritorio", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+//string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+//Criando um arquivo .json
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
+Console.WriteLine(serializado);
+
+//Data padrçao ISO 8601
+*/
+
+
+
+
+
+
+
+
+/*
 
 // IF  TERNARIO 
 
@@ -29,7 +84,7 @@ Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
 
 
 
-
+*/
 
 
 /*
